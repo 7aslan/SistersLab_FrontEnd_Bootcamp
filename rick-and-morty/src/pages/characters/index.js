5a6a4fc24619722d26a3c7ae16422b5dc244a943
Samet Aslan <sametaslan1710@gmail.com>
2023-11-10 04:18:40 +0300

    add: Week 4 assignment 3

--- rick-and-morty/src/pages/characters/index.js
+++ rick-and-morty/src/pages/characters/index.js
@@ -1,91 +1,75 @@
-import { useEffect , useState } from "react";
-import { fetchCharactersWithFetch } from "../FetchandAxios/fetch";
+import { useEffect, useState } from 'react';
+import { fetchCharacters } from '../api';
 import {
-    Autocomplete,
-    Button,
-    Card,
-    CardActions,
-    CardMedia,
-    CardContent,
-    IconButton,
-    Stack,
-    TextField,
-    Typography,
+  Button,
+  Card,
+  CardActions,
+  CardMedia,
+  CardContent,
+  IconButton,
+  Stack,
+  TextField,
+  Typography,
 } from '@mui/material';
-
 import FavoriteIcon from '@mui/icons-material/Favorite';
 
+const Characters = () => {
+  const [characters, setCharacters] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredCharacters, setFilteredCharacters] = useState([]);
 
+  const getCharacters = async () => {
+    const characters = await fetchCharacters();
+    setCharacters(characters);
+    setFilteredCharacters(characters);
+  };
 
-const Characters = () =>{
-    const [characters, setCharacters] = useState([]);
-    const [selectedCharacter, setSelectedCharacter] = useState(null);
-    const [error, setError] = useState(null);
-    
-    const getCharacters = async () =>{  
-      const characters = await fetchCharactersWithFetch();
-      setCharacters(characters);         
-    };
+  useEffect(() => {
+    getCharacters();
+  }, []);
 
-    useEffect(()=>{
-        getCharacters();
-    },[]);
-
-    const handleCharacterSelect = (e, value) => {
-        setSelectedCharacter(value);
-      };
-      console.log('selectedCharacter :>> ', selectedCharacter);
-    
-      const filteredCharacters = selectedCharacter
-        ? characters.filter((character) => character.id === selectedCharacter.id)
-        : characters;
-
-    return (
-        <Stack spacing={2}>
-        <Autocomplete
-          id="combo-box"
-          options={characters}
-          getOptionLabel={(character) => character.name}
-          onChange={handleCharacterSelect}
-          sx={{ width: 300 }}
-          size="small"
-          renderInput={(params) => <TextField {...params} label="Character" />}
-        />
-        <Stack
-          spacing={{ xs: 1, sm: 2 }}
-          direction="row"
-          useFlexGap
-          flexWrap="wrap"
-        >
-          {filteredCharacters.map((character) => (
-            <Card key={character.id} sx={{ width: '150px' }}>
-              <CardMedia
-                component="img"
-                alt="character"
-                height="140"
-                image={character.image}
-              />
-              <CardContent>
-                <Typography variant="h5" component="div">
-                  {character.name}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {character.gender} {character.status}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <IconButton aria-label="add to favorites">
-                  <FavoriteIcon color="error" />
-                </IconButton>
-                <Button size="small" variant="text">
-                  Details
-                </Button>
-              </CardActions>
-            </Card>
-          ))}
-        </Stack>
-      </Stack>
+  const handleSearch = () => {
+    const filteredSearch = characters.filter((character) =>
+      character.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
+    setFilteredCharacters(filteredSearch);
   };
   
+
+  return (
+    <Stack spacing={2}>
+      <TextField
+        label="Search Character"
+        variant="outlined"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        onBlur={handleSearch}
+      />
+      <Stack spacing={{ xs: 1, sm: 2 }} direction="row" useFlexGap flexWrap="wrap">
+        {filteredCharacters.map((character) => (
+          <Card key={character.id} sx={{ width: '150px' }}>
+            <CardMedia component="img" alt="character" height="140" image={character.image} />
+            <CardContent>
+              <Typography variant="h5" component="div">
+                {character.name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {character.gender} {character.status}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <IconButton aria-label="add to favorites">
+                <FavoriteIcon color="error" />
+              </IconButton>
+              <Button size="small" variant="text">
+                Details
+              </Button>
+            </CardActions>
+          </Card>
+        ))}
+      </Stack>
+    </Stack>
+  );
+};
+
 export default Characters;
